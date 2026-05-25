39162b7c13567b852f15523662c9f147af0aeaaf
DMCH <danhunt94@hotmail.com>
2026-05-25 13:03:19 +0100

    Added comments to temp-message.py

--- temp-message.py
+++ temp-message.py
@@ -1,20 +1,30 @@
+# Import Libraries
 from sense_hat import SenseHat
 
+# Initialise SenseHAT
 sense = SenseHat()
 sense.clear()
+
+# Set colours for LEDs
 blue = (0, 0, 255)
 green = (0, 255, 0)
 red = (255,0,0)
+
+# Set temperature factor - used to offset Pi temperature
 correction_factor = 1.7
+
+# Initialise main loop
 while True:
   temp = sense.get_temperature()
-  corrected_temp = temp/correction_factor
-  if corrected_temp >=26:
+  corrected_temp = temp/correction_factor   # Calculate corrected temperature using SenseHAT value and correction_factor
+  
+  # Print message using Pi LEDs based on corrected_temp value in relation to temp thresholds
+  if corrected_temp >=26: # Hot threshold
     sense.show_message(f"HOT! {corrected_temp:.1f}C", text_colour = red)
 
-  elif corrected_temp <= 18:
+  elif corrected_temp <= 18: # Cold threshold
     sense.show_message(f"COLD! {corrected_temp:.1f}C", text_colour = blue)
-  else:
+  else: # Any other temp threshold
     sense.show_message(f"Fine! {corrected_temp:.1f}C", text_colour = green)
   print(corrected_temp)
 
